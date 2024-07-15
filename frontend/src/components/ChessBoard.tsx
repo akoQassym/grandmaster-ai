dee5477b7b324563cf9bc90afbae651ccef473c0
Aknur Kassym <kassym.aknur@gmail.com>
2024-07-15 23:38:28 +0500

    feat: add params to the ChessBoardProps interface & add orientation & create styles

--- frontend/src/components/ChessBoard.tsx
+++ frontend/src/components/ChessBoard.tsx
@@ -3,21 +3,26 @@ import "chessground/assets/chessground.base.css";
 import "chessground/assets/chessground.brown.css";
 import "chessground/assets/chessground.cburnett.css";
 import { Key } from "chessground/src/types";
+import styled from 'styled-components';
 
 interface ChessBoardProps {
   fen: string;
   lastMove: Key[];
+  width: number;
+  height: number;
+  orientation: "white" | "black";
 }
 
-const ChessBoard: React.FC<ChessBoardProps> = ({ fen, lastMove }) => {
+const ChessBoard: React.FC<ChessBoardProps> = ({ fen, lastMove, width = 400, height = 400, orientation = 'white' }) => {
   return (
-    <div>
+    <ChessgroundWrapper $height={height} $width={width}>
       <Chessground
-        width={400}
-        height={400}
+        width={width}
+        height={height}
         contained={true}
         config={{
           fen: fen,
+          orientation: orientation,
           movable: {
             free: false,
             color: 'both',
@@ -31,8 +36,13 @@ const ChessBoard: React.FC<ChessBoardProps> = ({ fen, lastMove }) => {
           }
         }}
       />
-    </div>
+    </ChessgroundWrapper>
   );
 };
 
 export default ChessBoard;
+
+const ChessgroundWrapper = styled.div<{ $width: number, $height: number }>`
+  width: ${props => props.$width}px;
+  height: ${props => props.$height}px;
+`
