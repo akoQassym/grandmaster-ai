e594f443531e161bdf8c6e5a18eeecff4c0287a8
Aknur Kassym <kassym.aknur@gmail.com>
2024-07-15 23:39:52 +0500

    feat: add grouping games by date

--- frontend/src/components/GameList.tsx
+++ frontend/src/components/GameList.tsx
@@ -1,26 +1,20 @@
 import { useState } from 'react';
+import styled from 'styled-components';
 import axios from 'axios';
-
-interface Game {
-  id: string;
-  date: string;
-  opponent: string;
-  opponentRating: number, 
-  color: string;
-  status: string;
-  winner: string;
-  pgn: string;
-}
+import Button from './Button';
+import { Game } from '../types';
 
 interface GameListProps {
-  onSelectGame: (pgn: string, username: string) => void;
+  onSelectGame: (game: Game) => void;
 }
 
 const GameList: React.FC<GameListProps> = ({ onSelectGame }) => {
+  const [loading, setLoading] = useState<boolean>(false);
   const [lichessId, setLichessId] = useState('');
-  const [games, setGames] = useState<Game[]>([]);
+  const [groupedGames, setGroupedGames] = useState<{ [key: string]: Game[] }>({});
 
   const fetchGames = async () => {
+    setLoading(true);
     try {
       const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/lichess/${lichessId}`);
       const games = response.data.map((game: any) => {
@@ -32,7 +26,9 @@ const GameList: React.FC<GameListProps> = ({ onSelectGame }) => {
         return {
           id: game.id,
           date: new Date(game.createdAt).toLocaleString(),
-          opponent: opponent.user ? opponent.name : `AI Level ${opponent.aiLevel}`,
+          userId: lichessId,
+          opponentId: opponent.user ? opponent.user.id : null,
+          opponentName: opponent.user ? opponent.user.name : `AI Level ${opponent.aiLevel}`,
           opponentRating: opponent.user ? opponent.rating : opponent.aiLevel,
           color: isUserWhite ? 'white' : 'black',
           winner: game.winner,
@@ -40,10 +36,28 @@ const GameList: React.FC<GameListProps> = ({ onSelectGame }) => {
           pgn: game.pgn,
         };
       });
-      setGames(games);
+      setGroupedGames(groupGamesByDate(games));
     } catch (error) {
       console.error("Error fetching games:", error);
     }
+    setLoading(false);
+  };
+
+  const groupGamesByDate = (games: Game[]) => {
+    return games.reduce((groupedGames, game) => {
+      const date = new Date(game.date).toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+      });
+      
+      if (!groupedGames[date]) {
+        groupedGames[date] = [];
+      }
+      
+      groupedGames[date].push(game);
+      return groupedGames;
+    }, {} as { [key: string]: Game[] });
   };
 
   return (
@@ -54,22 +68,56 @@ const GameList: React.FC<GameListProps> = ({ onSelectGame }) => {
         value={lichessId}
         onChange={(e) => setLichessId(e.target.value)}
       />
-      <button onClick={fetchGames}>Fetch Games</button>
-      <ul>
-        {games.map((game) => (
-          <li key={game.id}>
-            <p>{game.date} </p>
-            <p>Opponent: {game.opponent} ({game.opponentRating})</p>
-            <p>You played: {game.color} </p>
-            <p>Winner: {game.winner} </p>
-            <p>Game status:{game.status} </p>
-            <p>Game PGN:{game.pgn} </p>
-            <button onClick={() => onSelectGame(game.pgn, lichessId)}>Analyze</button>
-          </li>
-        ))}
-      </ul>
+      <Button mode='secondary' onClick={fetchGames}>Fetch Games</Button>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <GameGroupWrapper>
+          {Object.keys(groupedGames).map((date) => (
+            <div key={date}>
+              <h2>{date}</h2>
+              <GameListWrapper>
+                {groupedGames[date].map((game) => (
+                  <GameContainer key={game.id}>
+                    <GameStatus $won={game.color === game.winner}>{game.color === game.winner ? 'Win' : 'Lose'}</GameStatus>
+                    <p>You ({game.color}) vs {game.opponentName} ({game.opponentRating})</p>
+                    <p>Game status: {game.status} </p>
+                    <Button mode='secondary' onClick={() => onSelectGame(game)}>Analyze</Button>
+                  </GameContainer>
+                ))}
+              </GameListWrapper>
+            </div>
+          ))}
+        </GameGroupWrapper>
+      )}
+      
     </div>
   );
 };
 
 export default GameList;
+
+const GameGroupWrapper = styled.div`
+  padding: 25px 0;
+`
+
+const GameListWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`
+
+const GameContainer = styled.div`
+  background: ${({ theme }) => theme.secondaryColor};
+  border-radius: 5px;
+  margin-right: 15px;
+  margin-bottom: 15px;
+  padding: 15px 10px;
+  box-sizing: border-box;
+  width: 230px;
+`
+
+const GameStatus = styled.p<{ $won?: boolean; }>`
+  font-weight: ${({ theme }) => theme.fontWeightBold};
+  color: ${props => props.$won ? props.theme.green : props.theme.red};
+`
