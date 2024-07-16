12577f9f77a97500aa16098d734e56ddc9c5a95b
Aknur Kassym <kassym.aknur@gmail.com>
2024-07-16 21:47:55 +0500

    feat: add analysis and explanations to the Game interface & move other types

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -9,4 +9,24 @@ export interface Game {
     status: string;
     winner: string;
     pgn: string;
-}
+    analysis?: Move[] | undefined;
+    explanations?: { [key: string]: string } | undefined;
+  }
+  
+  export interface Move {
+    uci_move: string;
+    best_move: string;
+    classification: "Brilliant" | "Excellent" | "Good" | "Inaccuracy" | "Mistake" | "Blunder" | "Missed Win" | "Neutral";
+    evaluation_before: number;
+    evaluation_after: number;
+    fen_before: string;
+    fen_after: string;
+    pv_after: Array<PV>;
+    pv_before: Array<PV>;
+  }
+
+  export interface PV {
+    Move: string;
+    Centipawn: number;
+    Mate: boolean | null;
+  }
